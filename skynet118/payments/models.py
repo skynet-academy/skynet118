c0da81486fbed581a1c78e4c5465bb9a7e248453
Nicolas <nico_8830_3@hotmail.com>
2021-07-26 23:11:09 +0300

    adding payment pages

--- skynet118/payments/models.py
+++ skynet118/payments/models.py
@@ -5,7 +5,9 @@ from django.db import models
 
 class Courses(models.Model):
     courses = models.CharField(max_length=200)
+    description = models.TextField(max_length=1000, default="")
     teacher = models.CharField(max_length=200)
+    image = models.ImageField(upload_to="images/")
     pub_date = models.DateTimeField("Date Published")
 
     def __str__(self):
