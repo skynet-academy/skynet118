3f698efabb3a71d8f59897999dfe6da31e837ec9
Nicolas <nicolassulca118@gmail.com>
2021-08-01 08:34:07 +0000

    changing the background

--- skynet118/skynet118/settings.py
+++ skynet118/skynet118/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-r=@4q(&bg7c=yn7%p-#c%xr##lz+e9(438+efw3^pe3c77yr82
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -127,7 +127,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, '/static/')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
